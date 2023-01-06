740ef4618c08763682194b49922ca227846da456
Kayky Casagrande <kayky.casagrande@unicred.com.br>
2023-01-06 09:35:56 -0300

    Initial Commit

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by unicred on 06/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
